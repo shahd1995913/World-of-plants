94f727da872ddba1ca02d5ced5d039a52bbb8e4e
Shahed Alkhateeb <83476666+shahd1995913@users.noreply.github.com>
2023-06-06 02:40:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,60 +30,81 @@ def preprocess_image(image):
     return keras.applications.mobilenet.preprocess_input(img_array)
 
 def main():
-    st.title("Image Classification")
-
-    # Set the overall page width
+    # Set page width and center content
+    max_width = 800
     st.markdown(
-        """
+        f"""
         <style>
-        .reportview-container .main {
-            max-width: 800px;
+        .reportview-container .main .block-container{{
+            max-width: {max_width}px;
+            padding-top: 1rem;
+            padding-bottom: 1rem;
             margin: 0 auto;
-        }
+        }}
         </style>
         """,
         unsafe_allow_html=True
     )
 
-    # Create columns for the image and predictions
-    col1, col2 = st.columns([1, 1.5])
+    st.title("Image Classification")
+    st.subheader("Upload an image for classification")
 
-    col1.markdown(
-        """
-        <div style='text-align: center;'>
-            <h1 style='color: #f63366;'>Image Classification</h1>
-            <p>Upload an image for classification.</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-    uploaded_file = col1.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
+    uploaded_file = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        col1.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
         # Preprocess the image
         processed_image = preprocess_image(image)
 
         # Make predictions
         predictions = model.predict(processed_image)
-        predicted_class_index = np.argmax(predictions)
+        predicted_class_index = predictions.argmax()
         predicted_class_name = class_names[predicted_class_index]
         confidence = predictions[0][predicted_class_index] * 100
 
-        col2.write("Predicted Class:")
-        col2.write(predicted_class_name)
+        # Display predicted class and confidence
+        st.markdown(
+            """
+            <div style='text-align: center;'>
+                <h2 style='font-weight: bold; color: #0072B2;'>Predicted Class</h2>
+                <h3 style='font-weight: bold; color: #0072B2;'>{}</h3>
+            </div>
+            """.format(predicted_class_name),
+            unsafe_allow_html=True
+        )
 
-        col2.write("Confidence:")
-        col2.write("{:.2f}%".format(confidence))
+        st.markdown(
+            """
+            <div style='text-align: center;'>
+                <h2 style='font-weight: bold; color: #0072B2;'>Confidence</h2>
+                <h3 style='font-weight: bold; color: #0072B2;'>{:.2f}%</h3>
+            </div>
+            """.format(confidence),
+            unsafe_allow_html=True
+        )
 
-        col2.write("Other Classes:")
+        # Display other classes
+        st.markdown(
+            """
+            <div style='text-align: center;'>
+                <h2 style='font-weight: bold; color: #0072B2;'>Other Classes</h2>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
         for i, class_name in enumerate(class_names):
             if i != predicted_class_index:
-                col2.write("{}: {:.2f}%".format(class_name, predictions[0][i] * 100))
-
+                st.markdown(
+                    """
+                    <div style='text-align: center;'>
+                        <h3 style='font-weight: bold; color: #0072B2;'>{}: {:.2f}%</h3>
+                    </div>
+                    """.format(class_name, predictions[0][i] * 100),
+                    unsafe_allow_html=True
+               
 
 
 if __name__ == '__main__':
