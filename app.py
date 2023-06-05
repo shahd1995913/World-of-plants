07fa28d5c00d50c3b685c1fa55fabd31193c4579
Shahed Alkhateeb <83476666+shahd1995913@users.noreply.github.com>
2023-06-06 02:48:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 import numpy as np
 from PIL import Image
 
-st.image("tomatoes.jpg", width=700)
+st.image("bacterial-spot-tomato.jpg", width=700)
 
 class_names = [
     'Tomato blight disease',
