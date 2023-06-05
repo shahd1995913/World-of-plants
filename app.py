9986e8e9db416ea66a43a58bf502a63c6281fe0b
Shahed Alkhateeb <83476666+shahd1995913@users.noreply.github.com>
2023-06-06 01:59:51 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Image Classification with Google's Teachable Machine")
+st.header("Brain Tumor MRI Classification Example")
+st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
