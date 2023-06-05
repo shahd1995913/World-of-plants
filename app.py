24c814f9f389f6c29dffc83c80832c85d10ffe7b
Shahed Alkhateeb <83476666+shahd1995913@users.noreply.github.com>
2023-06-06 02:30:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,46 +43,8 @@ class_names = [
     'Spider mites Two spotted spider mite'
 ]
 
-
-st.markdown(
-    """
-    <style>
-    .centered {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .title {
-        font-size: 32px;
-        font-weight: bold;
-        margin-bottom: 20px;
-    }
-    .image-container {
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-    .prediction-container {
-        margin-bottom: 20px;
-    }
-    .class-container {
-        margin-bottom: 10px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-import base64
-
-def image_to_base64(image):
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    return base64.b64encode(buffered.getvalue()).decode()
-
-
 def main():
     st.title("Image Classification")
-    st.markdown('<div class="title">Image Classification</div>', unsafe_allow_html=True)
     st.write("Upload an image for classification.")
 
     uploaded_file = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
@@ -90,7 +52,6 @@ def main():
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True, clamp=True)
-        st.markdown('<div class="image-container centered"><img src="data:image/png;base64,{}" width="300" /></div>'.format(image_to_base64(image)), unsafe_allow_html=True)
 
         # Preprocess the image
         processed_image = preprocess_image(image)
@@ -101,13 +62,17 @@ def main():
         predicted_class_name = class_names[predicted_class_index]
         confidence = predictions[0][predicted_class_index] * 100
 
-        st.markdown('<div class="prediction-container">Predicted Class: <strong>{}</strong></div>'.format(predicted_class_name), unsafe_allow_html=True)
-        st.markdown('<div class="prediction-container">Confidence: <strong>{:.2f}%</strong></div>'.format(confidence), unsafe_allow_html=True)
-        
-        st.markdown('<div class="class-container">Other Classes:</div>', unsafe_allow_html=True)
+        st.write("Predicted Class:", predicted_class_name)
+        st.write("Confidence: {:.2f}%".format(confidence))
+
+        st.write("Other Classes:")
         for i, class_name in enumerate(class_names):
             if i != predicted_class_index:
-                st.markdown('<div class="class-container">{}: {:.2f}%</div>'.format(class_name, predictions[0][i] * 100), unsafe_allow_html=True)
+                st.write("{}: {:.2f}%".format(class_name, predictions[0][i] * 100))
+
+
+
+
 
 if __name__ == '__main__':
     main()
