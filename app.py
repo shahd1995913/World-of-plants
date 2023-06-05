a6895b6486cd764ae319193ac472959c90dc0d0e
Shahed Alkhateeb <83476666+shahd1995913@users.noreply.github.com>
2023-06-06 02:21:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
-st.title("Image Classification with Google's Teachable Machine")
-st.header("Brain Tumor MRI Classification Example")
-st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
-
-import streamlit as st
+st.title("Image Classification")
+st.header("Upload a Image")
+st.text("Done by shahed Alhateeb 2023")
 import cv2
-import numpy as np
 from keras.models import load_model
-
-import streamlit as st
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -35,9 +30,6 @@ def preprocess_image(image):
     return keras.applications.mobilenet.preprocess_input(img_array)
 
 def main():
-   st.title("Image Classification")
-   st.write("Upload an image for classification.")
-
    uploaded_file = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
 
    if uploaded_file is not None:
